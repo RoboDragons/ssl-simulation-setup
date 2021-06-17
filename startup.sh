1ab6c949587924fc6ddc3b152efb258178be96c6
Kotet <kotet0418@gmail.com>
2021-06-17 18:09:56 +0900

    テスト用チームを追加

--- startup.sh
+++ startup.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 set -x
+./config/docker/init.sh
+./config/caddy/generate_caddyfile.py
 docker-compose up -d
 ./config/guacamole/update_guacamole.py
 ./config/caddy/update_caddy_passwords.sh
 ./config/caddy/generate_caddyfile.py
 ./config/caddy/update_caddy_config.sh
-docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons
+docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons-a
+docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons-b
 ./caddy/install_local_CA.sh
 
 echo "vnc password:"
