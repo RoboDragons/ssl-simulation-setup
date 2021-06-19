fe48ff6fa80d5e9a1b8d855d352c27fc8ffd5f32
Kotet <kotet0418@gmail.com>
2021-06-19 14:50:06 +0900

    nslookupをインストール

--- startup.sh
+++ startup.sh
@@ -3,12 +3,12 @@ set -x
 ./config/docker/init.sh
 ./config/caddy/generate_caddyfile.py
 docker-compose up -d
+sleep 30
 ./config/guacamole/update_guacamole.py
 ./config/caddy/update_caddy_passwords.sh
 ./config/caddy/generate_caddyfile.py
 ./config/caddy/update_caddy_config.sh
-docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons-a
-docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons-b
+docker-compose -f docker-compose-teams.yaml up -d --build team-robodragons team-robodragons-a team-robodragons-b
 ./caddy/install_local_CA.sh
 
 echo "vnc password:"
